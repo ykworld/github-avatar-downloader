ec85d9ced4fbffe9a08c797a444e9c7ff05b6e34
ykworld <yonseung.kim@gmail.com>
2017-06-05 17:05:12 +0000

    created the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,12 @@
 var request = require('request');
+var GITHUB_USER = "ykworld";
+var GITHUB_TOKEN = "8864bf92944159b9939ee3995f5850e8a9c76682";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
