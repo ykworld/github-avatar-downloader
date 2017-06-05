6df37a7443951800aa6ad54c4a8b6e6ff384e92b
ykworld <yonseung.kim@gmail.com>
2017-06-05 20:01:31 +0000

    added error handler

--- download_avatars.js
+++ download_avatars.js
@@ -3,10 +3,23 @@ var request = require('request');
 var fs = require('fs');
 var params = process.argv.slice(2);
 var GITHUB_USER = "ykworld";
+var imageFolder = "avatars";
+
+if (!fs.existsSync("./.env")) {
+  console.log("The .env file is missing");
+  return;
+}
+
 var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 if (params[0] == undefined || params[1] == undefined) {
   console.log("Please enter two arguments!");
+  return;
+} else if (params.length > 2) {
+  console.log("An incorrect number of arguments given to program");
+  return;
+} else if (GITHUB_TOKEN == undefined) {
+  console.log('The .env file is missing information');
   return;
 }
 
@@ -22,6 +35,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 
   request(requestOptions, function (error, response, body) {
+    if (response.statusCode === 404) {
+      console.log("The provided owner/repo does not exist");
+      return;
+    } else if (response.statusCode === 401) {
+      console.log("The .env file contains incorrect credentials");
+      return;
+    }
     cb(error, JSON.parse(body));
   });
 }
@@ -35,11 +55,19 @@ function downloadImageByURL(url, filePath) {
 }
 
 getRepoContributors(params[0], params[1], function(err, result) {
-  console.log("Errors:", err);
+  if (err) {
+    console.log("Errors:", err);
+    return;
+  }
+
+  if (!fs.existsSync(imageFolder)) {
+    console.log("The folder to store images to does not exist");
+    return;
+  }
 
   for (idx in result) {
     var url = result[idx].avatar_url;
-    var filePath = "avatars/" + result[idx].login + ".jpg";
+    var filePath = imageFolder + "/" + result[idx].login + ".jpg";
     downloadImageByURL(url, filePath);
   }
 });
